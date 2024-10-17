c3aea616508ce2bc5f9c761739f64510fdf4c7f6
FedericoDeniard <fededeniard@gmail.com>
2024-10-17 18:42:28 -0300

    "first"

--- src/database/querys.ts
+++ src/database/querys.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-import { capitalize } from "../utils/string.js";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+const PAGE_SIZE = 10;
 
 type ProductBrandSchema = {
   product_id: number;
@@ -11,12 +11,15 @@ export type productSchema = {
   name: string;
   description: string;
   img_url: string;
+  price: number;
   product_brand: ProductBrandSchema[];
 };
 
 export const getProducts = async () => {
   try {
-    const products = await prisma.product.findMany();
+    const products = await prisma.product.findMany({
+      include: { product_brand: { include: { brand: true } } },
+    });
     return products;
   } catch (e) {
     console.error(e);
@@ -24,19 +27,13 @@ export const getProducts = async () => {
   }
 };
 
-export const getProductByBrand = async (brandName: string) => {
+export const getProductByProductBrand = async ({ product_id, brand_id }) => {
   try {
-    const products = await prisma.product.findMany({
+    const products = await prisma.product_Brand.findFirst({
       where: {
-        Product_Brand: {
-          some: {
-            brand: {
-              name: brandName,
-            },
-          },
-        },
+        brand_id: parseInt(brand_id),
+        product_id: parseInt(product_id),
       },
-      include: { Product_Brand: true },
     });
     return products;
   } catch (e) {
@@ -57,22 +54,40 @@ export const createProduct = async (
     if (!brand) {
       throw new Error("Brand not found");
     }
-    const newProduct = await prisma.product.create({
-      data: {
-        name: product.name,
-        description: product.description,
-        img_url: product.img_url,
-        Product_Brand: {
-          create: [
-            {
-              brand: { connect: { id: brand.id } },
-            },
-          ],
-        },
-      },
-      include: { Product_Brand: true },
+    let existingProduct = await prisma.product.findUnique({
+      where: { name: product.name },
     });
-    return newProduct;
+    if (existingProduct) {
+      const updatedProduct = await prisma.product_Brand.create({
+        data: {
+          product: { connect: { id: existingProduct.id } },
+          brand: { connect: { id: brand.id } },
+          description: product.description,
+          img_url: product.img_url,
+          price: product.price,
+        },
+        include: { product: true, brand: true },
+      });
+      return updatedProduct;
+    } else {
+      const newProduct = await prisma.product.create({
+        data: {
+          name: product.name,
+          product_brand: {
+            create: [
+              {
+                brand: { connect: { id: brand.id } },
+                description: product.description,
+                img_url: product.img_url,
+                price: product.price,
+              },
+            ],
+          },
+        },
+        include: { product_brand: true },
+      });
+      return newProduct;
+    }
   } catch (e) {
     console.error(e);
     throw e;
@@ -84,9 +99,14 @@ export type BrandSchema = {
   logo_url: string;
 };
 
-export const getBrands = async () => {
+export const getBrands = async (cursor?: number) => {
   try {
-    const brands = await prisma.brand.findMany();
+    const brands = await prisma.brand.findMany({
+      take: PAGE_SIZE,
+      cursor: cursor ? { id: cursor } : undefined,
+      skip: cursor ? 1 : 0,
+      orderBy: { id: "asc" },
+    });
     return brands;
   } catch (e) {
     console.error(e);
@@ -100,6 +120,133 @@ export const createBrand = async (brand: BrandSchema) => {
       data: brand,
     });
     return newBrand;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+export const getProductsByFilters = async (
+  all?: string,
+  product_name?: string,
+  brand_name?: string[],
+  description?: string
+) => {
+  try {
+    const filterName: Prisma.ProductWhereInput = {
+      OR: [],
+    };
+
+    if (all) {
+      filterName.OR.push({
+        name: {
+          contains: all,
+        },
+      });
+    }
+
+    if (product_name) {
+      filterName.OR.push({
+        name: {
+          contains: product_name,
+        },
+      });
+    }
+
+    const products = await prisma.product.findMany({
+      where: filterName,
+      include: {
+        product_brand: {
+          include: {
+            brand: true,
+          },
+        },
+      },
+    });
+
+    return products;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const getProductByString = async (filter: string) => {
+  try {
+    const productsByName = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: filter,
+        },
+      },
+      include: {
+        product_brand: {
+          include: {
+            brand: true,
+          },
+        },
+      },
+    });
+
+    const productsByDescriptionOrBrand = await prisma.product.findMany({
+      where: {
+        OR: [
+          {
+            product_brand: {
+              some: {
+                brand: {
+                  name: {
+                    contains: filter,
+                  },
+                },
+              },
+            },
+          },
+          {
+            product_brand: {
+              some: {
+                description: {
+                  contains: filter,
+                },
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        product_brand: {
+          include: {
+            brand: true,
+          },
+          where: {
+            OR: [
+              {
+                brand: {
+                  name: {
+                    contains: filter,
+                  },
+                },
+              },
+              {
+                description: {
+                  contains: filter,
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const combinedResults = [
+      ...productsByName,
+      ...productsByDescriptionOrBrand,
+    ];
+
+    const uniqueProducts = Array.from(
+      new Set(combinedResults.map((product) => product.id))
+    ).map((id) => combinedResults.find((product) => product.id === id));
+
+    return uniqueProducts;
   } catch (e) {
     console.error(e);
     throw e;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import {
   createBrand,
-  getProducts,
   getBrands,
   createProduct,
   productSchema,
-  getProductByBrand,
+  getProductsByFilters,
+  getProductByString,
+  getProductByProductBrand,
 } from "./database/querys.js";
+
+import cors from "cors";
 
 import { capitalize } from "./utils/string.js";
 
@@ -15,13 +18,49 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const whitelist = ["http://localhost:5173"];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+
+const corsOptions = {
+  origin: true,
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/products", async (req, res) => {
+// app.get("/products", async (req, res) => {
+//   const { brand_name, product_name, description, all } = req.query;
+//   const brandNamesArray = brand_name ? brand_name.split(",") : undefined;
+
+//   try {
+//     let products = await getProductsByFilters(
+//       brandNamesArray,
+//       product_name,
+//       description,
+//       all
+//     );
+//     res.status(200).json(products);
+//   } catch (e) {
+//     console.error(e);
+//     res.status(500).json(e);
+//   }
+// });
+
+app.get("/products/:product_id/:brand_id", async (req, res) => {
+  let { product_id, brand_id } = req.params;
   try {
-    let products = await getProducts();
+    let products = await getProductByProductBrand({ product_id, brand_id });
     res.status(200).json(products);
   } catch (e) {
     console.error(e);
@@ -29,26 +68,34 @@ app.get("/products", async (req, res) => {
   }
 });
 
-app.get("/products/:brand_name", async (req, res) => {
-  let { brand_name } = req.params;
-  brand_name = capitalize(brand_name);
-  try {
-    let products = await getProductByBrand(brand_name);
-    res.status(200).json(products);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send(e);
-  }
-});
+// app.get(
+//   "/products/:brand_name?/:product_name?/:description?/",
+//   async (req, res) => {
+//     const { brand_name, product_name, description } = req.query;
+//     const brandNamesArray = brand_name ? brand_name.split(",") : undefined;
+//     try {
+//       const products = await getProductsByFilters(
+//         brandNamesArray,
+//         product_name,
+//         description
+//       );
+//       res.status(200).json(products);
+//     } catch (e) {
+//       console.error(e);
+//       res.status(500).send(e);
+//     }
+//   }
+// );
 
 app.post("/newProduct", async (req, res) => {
-  let { name, description, img_url, brand_name } = req.body;
+  let { name, description, img_url, brand_name, price } = req.body;
   name = capitalize(name);
   brand_name = capitalize(brand_name);
   const product: productSchema = {
     name,
     description,
     img_url,
+    price,
     product_brand: [],
   };
 
@@ -63,7 +110,8 @@ app.post("/newProduct", async (req, res) => {
 
 app.get("/brands", async (req, res) => {
   try {
-    let brands = await getBrands();
+    const { cursor } = req.query;
+    let brands = await getBrands(cursor ? parseInt(cursor) : undefined);
     res.status(200).json(brands);
   } catch (e) {
     console.error(e);
