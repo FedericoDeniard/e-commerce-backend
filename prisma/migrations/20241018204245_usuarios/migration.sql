76ca77faf69fce76e9e1c7ca96f1d209881dd043
FedericoDeniard <fededeniard@gmail.com>
2024-10-18 17:43:43 -0300

    new users table

---
+++ prisma/migrations/20241018204245_usuarios/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL
+);
